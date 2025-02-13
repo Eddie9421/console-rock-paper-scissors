f49eb30e55513a4cf9c7a895bc055c8fda22f04e
Eddie <Eddie11910@outlook.com>
2025-02-12 20:44:08 -0800

    Created playGame() function and finished the exercise

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function generateRandomNumAToB(minimumBound, maximumBound)
 {
     return Math.floor(Math.random() * (maximumBound - minimumBound + 1) + minimumBound);
@@ -25,64 +22,78 @@ function getHumanChoice()
     return prompt("Rock, paper, or scissors?").toLowerCase();
 }
 
-function playRound(humanChoice, computerChoice) 
+
+function playGame()
 {
-    switch (humanChoice)
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) 
     {
-        case "rock":
-            if (computerChoice == "scissors")
-            {
-                console.log("You win. Rock beats scissors.");
-                humanScore++;
-            }
-            else if (computerChoice == "paper")
-            {
-                console.log("You lose. Paper beats rock.");
-                computerScore++;
-            }
-            else
-            {
-                console.log("No winner.");
-            }
-            break;
+        console.log(`You threw ${humanChoice}.`);
+        console.log(`Computer threw ${computerChoice}.`);
+        switch (humanChoice)
+        {
+            case "rock":
+                if (computerChoice == "scissors")
+                {
+                    console.log("You win. Rock beats scissors.");
+                    humanScore++;
+                }
+                else if (computerChoice == "paper")
+                {
+                    console.log("You lose. Paper beats rock.");
+                    computerScore++;
+                }
+                else
+                {
+                    console.log("No winner.");
+                }
+                break;
+    
+            case "paper":
+                if (computerChoice == "rock")
+                {
+                    console.log("You win. Paper beats rock.");
+                    humanScore++;
+                }
+                else if (computerChoice == "scissors")
+                {
+                    console.log("You lose. Scissors beats paper.");
+                    computerScore++;
+                }
+                else
+                {
+                    console.log("No winner.");
+                }
+                break;
+    
+            case "scissors":
+                if (computerChoice == "paper")
+                {
+                    console.log("You win. Scissors beats paper.");
+                    humanScore++;
+                }
+                else if (computerChoice == "rock")
+                {
+                    console.log("You lose. Rock beats scissors.");
+                    computerScore++;
+                }
+                else
+                {
+                    console.log("No winner.");
+                }
+                break;
+        }
+    }
 
-        case "paper":
-            if (computerChoice == "rock")
-            {
-                console.log("You win. Paper beats rock.");
-                humanScore++;
-            }
-            else if (computerChoice == "scissors")
-            {
-                console.log("You lose. Scissors beats paper.");
-                computerScore++;
-            }
-            else
-            {
-                console.log("No winner.");
-            }
-            break;
-
-        case "scissors":
-            if (computerChoice == "paper")
-            {
-                console.log("You win. Scissors beats paper.");
-                humanScore++;
-            }
-            else if (computerChoice == "rock")
-            {
-                console.log("You lose. Rock beats scissors.");
-                computerScore++;
-            }
-            else
-            {
-                console.log("No winner.");
-            }
-            break;
+    for (let i = 0; i < 5; i++)
+    {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        
+        playRound(humanSelection, computerSelection);
     }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+playGame();
