a324758768f38a154040c87c4b2e05da4b56fdfe
Eddie <Eddie11910@outlook.com>
2025-02-17 23:13:18 -0800

    Finished the UI

--- script.js
+++ script.js
@@ -29,69 +29,85 @@ function playRound(humanChoice, computerChoice)
 {
     const playerActionDiv = document.querySelector(".player-action");
     const computerActionDiv = document.querySelector(".computer-action");
-    const resultDiv = document.querySelector(".result");
+    const roundResultDiv = document.querySelector(".round-result");
+    const scoreDiv = document.querySelector(".score");
+    const gameResultDiv = document.querySelector(".game-result");
     playerActionDiv.textContent = `You threw ${humanChoice}.`;
     computerActionDiv.textContent = `Computer threw ${computerChoice}.`;
-
+    scoreDiv.textContent = `Player score: ${humanScore} | Computer score: ${computerScore}`;
     switch (humanChoice)
     {
         case "rock":
             if (computerChoice == "scissors")
             {
-                resultDiv.textContent = "You win. Rock beats scissors.";
+                roundResultDiv.textContent = "You win. Rock beats scissors.";
                 humanScore++;
             }
             else if (computerChoice == "paper")
             {
-                resultDiv.textContent = "You lose. Paper beats rock.";
+                roundResultDiv.textContent = "You lose. Paper beats rock.";
                 computerScore++;
             }
             else
             {
-                resultDiv.textContent = "No winner.";
+                roundResultDiv.textContent = "No winner.";
             }
             break;
 
         case "paper":
             if (computerChoice == "rock")
             {
-                resultDiv.textContent = "You win. Paper beats rock.";
+                roundResultDiv.textContent = "You win. Paper beats rock.";
                 humanScore++;
             }
             else if (computerChoice == "scissors")
             {
-                resultDiv.textContent = "You lose. Scissors beats paper.";
+                roundResultDiv.textContent = "You lose. Scissors beats paper.";
                 computerScore++;
             }
             else
             {
-                resultDiv.textContent = "No winner.";
+                roundResultDiv.textContent = "No winner.";
             }
             break;
 
         case "scissors":
             if (computerChoice == "paper")
             {
-                resultDiv.textContent = "You win. Scissors beats paper.";
+                roundResultDiv.textContent = "You win. Scissors beats paper.";
                 humanScore++;
             }
             else if (computerChoice == "rock")
             {
-                resultDiv.textContent = "You lose. Rock beats scissors.";
+                roundResultDiv.textContent = "You lose. Rock beats scissors.";
                 computerScore++;
             }
             else
             {
-                resultDiv.textContent = "No winner.";
+                roundResultDiv.textContent = "No winner.";
             }
             break;
+            
+    }
+    scoreDiv.textContent = `Player score: ${humanScore} | Computer score: ${computerScore}`;
+    if (humanScore == 5)
+    {
+        gameResultDiv.textContent = "You won.";
+    }
+    else if (computerScore == 5)
+    {
+        gameResultDiv.textContent = "Computer won.";
     }
 }
 
 window.addEventListener("load", () => {
     const optionsDiv = document.querySelector(".options");
     optionsDiv.addEventListener("click", (event) => {
-        playRound(event.target.textContent.toLowerCase(), getComputerChoice());
+        if (humanScore < 5 && computerScore < 5)
+        {
+            playRound(event.target.textContent.toLowerCase(), getComputerChoice());
+        }
+        
     });
     // const rockButton = document.querySelector("#rock-button");
     // const paperButton = document.querySelector("#paper-button");
